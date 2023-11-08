c05baaa778efa3b3db7fbc0df5f27a47acd62235
atar1boy <atar1boy@yandex.ru>
2023-11-08 19:20:30 +0300

    Настроены миграции

---
+++ alembic/versions/215991f66a16_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 215991f66a16
+Revises: 103fc4afafd0
+Create Date: 2023-11-08 19:08:51.801388
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '215991f66a16'
+down_revision = '103fc4afafd0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
